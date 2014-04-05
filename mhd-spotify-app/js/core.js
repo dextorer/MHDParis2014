2d7a3cfa235ad3466bd35ac80b3b766685019814
Sebastiano Gottardo <dextorer@gmail.com>
2014-04-05 17:31:59 +0200

    merged with new stuff

--- mhd-spotify-app/js/core.js
+++ mhd-spotify-app/js/core.js
@@ -1,15 +1,23 @@
-function fuckme() {
-	$.get('https://raw.githubusercontent.com/mlegenhausen/node-ogone-directlink/master/package.json',function(data,status) {
+var endpoint = 'https://raw.githubusercontent.com/mlegenhausen/node-ogone-directlink/master/package.json';
+
+function start() {
+	
+	// load spinner
+	var image = $('<img src="/img/spinner.gif" />'); // preload the spinner
+  	// $('#mButton').hide();
+  	// $('#spinner').show();
+
+  	// perform network call
+	$.get(endpoint, function(data,status) {
     	// console.log(data);
     	var obj = $.parseJSON(data);
-    	alert(obj.author);
+    	// alert(obj.author);
+
+		// restore button
+    	// $('#spinner').hide();
+    	// $('#mButton').show();
+
 	},'html');
 };
 
-function start() {
-	$(document).ready(function() {
-      	var contained = $('#txtarea').val();
-        alert(contained);
-    });
-}
 
